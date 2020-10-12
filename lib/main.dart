fb3ee0949c31661659ae58d01f0edbcaf16f261a
firegod17 <fire.god.47@gmail.com>
2020-10-12 17:00:27 +0300

    quick start and fix "hot reload"

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
+void main() => runApp(FlutterTutorialWidget());
+
+class FlutterTutorialWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: "FlutterTest",
       home: Scaffold(
         appBar: AppBar(
@@ -19,15 +22,17 @@ void main() {
             decoration: TextDecoration.underline,
             letterSpacing: 3.0,
             color: Colors.white,
+            fontFamily: "Slabo",
           ),
         ),
         floatingActionButton: FloatingActionButton(
           child: Text("ADD"),
           backgroundColor: Colors.grey[900],
+          onPressed: () {},
         ),
         backgroundColor: Colors.grey[600],
         // bottomNavigationBar: BottomNavigationBar(),
       ),
-    ),
-  );
+    );
+  }
 }
