883bdfec0174bc9ccdbc62457faafffe0263101a
firegod17 <fire.god.47@gmail.com>
2020-10-13 15:07:26 +0300

    container and background

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,74 @@ class FlutterTutorialWidget extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[900],
         ),
-        body: Padding(
-          padding: EdgeInsets.all(20),
-          child: IconButton(
-            icon: Icon(
-              Icons.search,
+        body: Container(
+          child: Container(
+            // color: Colors.black,
+            // margin: EdgeInsets.all(20),
+            // padding: EdgeInsets.all(30),
+            // transform: Matrix4.rotationZ(0.1),
+            child: IconButton(
+              icon: Icon(
+                Icons.search,
+              ),
+              iconSize: 50.0,
+              color: Colors.white,
+              onPressed: () {
+                print("New Info add");
+              },
             ),
-            iconSize: 50.0,
-            color: Colors.white,
-            onPressed: () {
-              print("New Info add");
-            },
+            decoration: BoxDecoration(
+              color: Colors.black,
+              // borderRadius: BorderRadius.circular(20),
+              shape: BoxShape.circle,
+              border: Border.all(
+                width: 2,
+                color: Colors.white,
+                style: BorderStyle.solid,
+              ),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black,
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(5, 5)),
+              ],
+            ),
+          ),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/img1.jpg"),
+              fit: BoxFit.fill,
+            ),
+            // gradient: RadialGradient(
+            //   colors: [
+            //     Colors.black,
+            //     Colors.white,
+            //   ],
+            // ),
           ),
         ),
+        //  Padding(
+        //   padding: EdgeInsets.all(20),
+        //   child: IconButton(
+        //     icon: Icon(
+        //       Icons.search,
+        //     ),
+        //     iconSize: 50.0,
+        //     color: Colors.white,
+        //     onPressed: () {
+        //       print("New Info add");
+        //     },
+        //   ),
+        // ),
         // SvgPicture.asset("assets/images/im3.svg"),
         floatingActionButton: FloatingActionButton(
           child: Text("ADD"),
           backgroundColor: Colors.grey[900],
           onPressed: () {},
         ),
-        backgroundColor: Colors.grey[600],
+        backgroundColor: Colors.grey[500],
         // bottomNavigationBar: BottomNavigationBar(),
       ),
     );
